b1557831eae041edd2eeead5067abc2a92021499
flor4 <salanson.flora@outlook.fr>
2026-02-02 10:41:24 +0100

    feat: url structure file

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for whichmovietonight project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/movies/', include('movies.urls')),
+    path('api/ratings/', include('ratings.urls')),
+    path('api/comments/', include('comments.urls')),
+    path('api/auth/', include('authentication.urls')),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
