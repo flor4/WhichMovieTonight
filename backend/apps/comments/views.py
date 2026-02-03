ffa220c458a1bad92a80bb8f692ebfe976570483
flor4 <salanson.flora@outlook.fr>
2026-02-03 18:40:19 +0100

    Chore: CRUD operations and permissions for comments API

--- backend/apps/comments/views.py
+++ backend/apps/comments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permisssions
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+class CommentViewSet(viewsets.ModelViewSet):
+    
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerReadOnly]
+
+    def get_queryset(self):
+
+        queryset = Comment.objects.select_related('user', 'movie')
+        movie_id = self.request.query_params.get('movie_id', None)
+        if movie_id:
+            queryset = queryset.filter(movie_id=movie_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
