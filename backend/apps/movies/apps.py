0f821040864816c4aaa30e39db91de9b4459c084
flor4 <salanson.flora@outlook.fr>
2026-02-18 11:48:36 +0100

    Fix: path in MoviesConfig name

--- backend/apps/movies/apps.py
+++ backend/apps/movies/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movies'
+    name = 'apps.movies'
     verbose_name = "Movies"
