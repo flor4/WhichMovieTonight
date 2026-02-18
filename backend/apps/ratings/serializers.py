175a8b08786078608e91ba4afe523c6537d075fa
flor4 <salanson.flora@outlook.fr>
2026-02-18 11:16:43 +0100

    Fix: typo

--- backend/apps/ratings/serializers.py
+++ backend/apps/ratings/serializers.py
@@ -6,5 +6,5 @@ class RatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rating
-        fiels = ['id', 'movie', 'user', 'user_username', 'score', 'created_at', 'updated_at']
+        fields = ['id', 'movie', 'user', 'user_username', 'score', 'created_at', 'updated_at']
         read_only_fields = ['user', 'created_at', 'updated_at']
